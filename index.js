8fa9ee1c0044e3dde65ac0f4bddfb46fa502d135
Jonathan Clem <jonathan@jclem.net>
2014-08-13 18:49:44 -0400

    accept prefix option in #write

--- index.js
+++ index.js
@@ -204,6 +204,10 @@ module.exports.write = function write(destPath, options) {
       comment = commentFormatter(path.join(path.relative(path.dirname(file.path), file.base), destPath, file.relative) + '.map');
       // fix paths for Windows
       comment = comment.replace('\\', '/');
+
+      if (options.prefix) {
+        comment = comment.replace('sourceMappingURL=', 'sourceMappingURL=' + options.prefix);
+      }
     }
 
     // append source map comment
