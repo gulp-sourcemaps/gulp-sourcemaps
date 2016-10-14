14fd48317d3458b917855bc1b6f833fafa8c93a4
nmccready <nemtcan@gmail.com>
2016-10-14 11:27:03 -0400

    attempt to avert disaster issue 246

---
+++ test/publish.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var test = require('tape');
+var exec = require('child_process').exec;
+
+function cleanUp(cb) {
+  return exec('rm -rf ./tmp', cb);
+}
+
+function makeTestPackage(cb) {
+  return exec('./scripts/mockPublish', cb);
+}
+
+// with regards to averting npm publishing disasters https://github.com/floridoo/gulp-sourcemaps/issues/246
+test('can load a published version', function(t) {
+  // return t.fail("mock fail");
+  cleanUp(function() {
+    makeTestPackage(function() {
+      try {
+        // attempt to load a packed / unpacked potential deployed version
+        require('../tmp/package/index');
+      }
+      catch (error){
+        t.fail(error);
+      }
+      finally{
+        cleanUp(function() {
+          t.end();
+        });
+      }
+    });
+  });
+});
