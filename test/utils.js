144b1344342e421ea9f6d71c84a9be334003dee1
Blaine Bublitz <blaine.bublitz@gmail.com>
2019-02-22 23:59:25 -0700

    update utils tests

--- test/utils.js
+++ test/utils.js
@@ -1,31 +1,34 @@
 'use strict';
 
-var test = require('tape');
+var expect = require('expect');
 var utils = require('../src/utils');
 
-test('exceptionToString: takes message if present', function(t) {
-  var exception = { message: 'exception message' };
-  var result = utils.exceptionToString(exception);
+describe('utils', function() {
 
-  t.equal(result, 'exception message');
-  t.end();
-});
+  it('exceptionToString: takes message if present', function(done) {
+    var exception = { message: 'exception message' };
+    var result = utils.exceptionToString(exception);
 
-test('exceptionToString: returns empty string if message is not present', function(t) {
-  var exception = { foo: 'bar' };
-  var result = utils.exceptionToString(exception);
-
-  t.equal(result, '');
-  t.end();
-});
-
-test('getCommentFormatter: gets a commenter with invalid extension', function(t) {
-  var commenter = utils.getCommentFormatter({
-    relative: 'some.junk',
-    contents: "var a = 'hello';",
-    sourceMap: { preExistingComment: true },
+    expect(result).toEqual('exception message');
+    done();
   });
 
-  t.true(commenter);
-  t.end();
+  it('exceptionToString: returns empty string if message is not present', function(done) {
+    var exception = { foo: 'bar' };
+    var result = utils.exceptionToString(exception);
+
+    expect(result).toEqual('');
+    done();
+  });
+
+  it('getCommentFormatter: gets a commenter with invalid extension', function(done) {
+    var commenter = utils.getCommentFormatter({
+      relative: 'some.junk',
+      contents: "var a = 'hello';",
+      sourceMap: { preExistingComment: true },
+    });
+
+    expect(commenter).toExist();
+    done();
+  });
 });
